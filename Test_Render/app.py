c93bde5fac82d935714ad02428e2479c8b9fee32
Guedes1742 <jairo-guedes@hotmail.com>
2025-10-17 22:37:35 -0300

    Update app.py 4

--- Test_Render/app.py
+++ Test_Render/app.py
@@ -19,7 +19,11 @@ def run_process():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render define a porta em uma variável
+    app.run(host='0.0.0.0', port=port)
+   
+
 
 
 
